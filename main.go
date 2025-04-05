3d38d23838959de27fc4374c7e569fbac4b57207
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-05 14:01:34 +0200

    Affichage des todos

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"GoToDo/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// creation du serveur Gin
 	r := gin.Default()
 
 	// definition des routes pour chaque action
-	// 1 - Generation d'un statique => GET
-	// 2 - Montrer les todo => GET
-	// 3 - Creation d'une todo => POST
-	// 4 - Modifier une todo => PUT
-	// 5 - Supprimer une todo => DELETE
+	// route affichage des todo
+	r.GET("/todo", services.GetTodos)
 
 	// lancement du serveur
 	r.Run(":8080")
--- services/todoHandler.go
+++ services/todoHandler.go
@@ -1,1 +1,30 @@
 package services
+
+import (
+	"GoToDo/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// stockage des todo
+// on utilise un slice de Todo et on l'initialize vide
+// on utilise le struct definis dans les models
+var todos = []models.Todo{}
+
+// 1 - Generation d'un statique => GET
+
+// 2 - Montrer les todo => GET
+func GetTodos(c *gin.Context) {
+	// on viens retourner les differentes todo -> proviens de la DB
+	// ici on utilise du JSON car on ne stocke pas les donnees.
+	// ici on retourne le sclice de todo qui contiens toutes les todo creer.
+	// on peut passer le code ou le http.StatusOK pour 200
+	c.JSON(http.StatusOK, todos)
+}
+
+// 3 - Creation d'une todo => POST
+
+// 4 - Modifier une todo => PUT
+
+// 5 - Supprimer une todo => DELETE
