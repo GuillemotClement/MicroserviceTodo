0c01194f53e0f061a5925151c8c0049d6b6084ce
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-05 13:50:46 +0200

    Preparation des routes

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	// creation du serveur Gin
+	r := gin.Default()
+
+	// definition des routes pour chaque action
+	// 1 - Generation d'un statique => GET
+	// 2 - Montrer les todo => GET
+	// 3 - Creation d'une todo => POST
+	// 4 - Modifier une todo => PUT
+	// 5 - Supprimer une todo => DELETE
+
+	// lancement du serveur
+	r.Run(":8080")
 }
